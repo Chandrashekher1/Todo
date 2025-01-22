9d932bc0f622e60561c359bc4691dd9628c1eae1
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-01-22 23:57:07 +0530

    Update login nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
         <div>
             <ul className='flex'>
                 <li className='mx-8 cursor-pointer'>Setting</li>
-                <li className='mx-8 cursor-pointer'>Profile </li>
+                <Link to="/login"><li className='mx-8 cursor-pointer'>Profile </li></Link>
             </ul>
         </div>
     </div>
