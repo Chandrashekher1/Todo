55718b682ab869612799e496c09a96fae4626ca1
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-01-25 17:32:23 +0530

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Header from './Components/Header';
 import Body from './Components/Body';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import ProfilePage from './Components/ProfilePage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/profile' element={<ProfilePage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
