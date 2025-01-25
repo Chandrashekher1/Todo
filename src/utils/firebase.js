6a9444ce380dde4da04a165ef2901a633924fe28
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-01-25 17:33:41 +0530

    add firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { api_key } from "./constant";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAq8eVfT1WijuyoYcqIazFdqzSRIclwcWw",
+  apiKey: api_key,
   authDomain: "todo1-5e7a8.firebaseapp.com",
   projectId: "todo1-5e7a8",
   storageBucket: "todo1-5e7a8.firebasestorage.app",
